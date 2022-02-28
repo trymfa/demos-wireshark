0675c33dffd3e7a7bde1ef077ea6f6999730b506
trymfa <trymfalkum@gmail.com>
2022-02-28 17:56:12 +0100

    tcp

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/trymfa/demos-wireshark
+
+go 1.17
---
+++ tcpclient.go
@@ -0,0 +1,43 @@
+package main
+import (
+	"net"
+	"os"
+	"time"
+)
+
+func main() {
+	strEcho := "Halo"
+	servAddr := "localhost:5000"
+	tcpAddr, err := net.ResolveTCPAddr("tcp", servAddr)
+	if err != nil {
+		println("ResolveTCPAddr failed:", err.Error())
+		os.Exit(1)
+	}
+
+	conn, err := net.DialTCP("tcp", nil, tcpAddr)
+	if err != nil {
+		println("Dial failed:", err.Error())
+		os.Exit(1)
+	}
+
+	_, err = conn.Write([]byte(strEcho))
+	if err != nil {
+		println("Write to server failed:", err.Error())
+		os.Exit(1)
+	}
+
+	println("write to server = ", strEcho)
+
+	reply := make([]byte, 1024)
+
+	_, err = conn.Read(reply)
+	//time.Sleep(2 * time.Second)
+	if err != nil {
+		println("Read from server failed:", err.Error())
+		os.Exit(1)
+	}
+	time.Sleep(2 * time.Second)
+	println("reply from server=", string(reply))
+
+	conn.Close()
+}
